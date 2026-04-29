209e8608049cf2ea73f6d2232284035ba41f33c9
dixar111 <148251928+dixar111@users.noreply.github.com>
2026-04-29 17:02:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    message = os.getenv("APP_MESSAGE", "Varsayılan mesaj")
-    return f"<h1>{message}</h1>"
+    message = os.getenv("APP_MESSAGE", "Codespaces Docker çalışıyor")
+    return render_template("index.html", message=message)
+
+@app.route("/about")
+def about():
+    return "<h2>Bu proje Docker + Flask PoC çalışmasıdır.</h2>"
 
 app.run(host="0.0.0.0", port=5000)
